c486a5a84b95bd86887e2862cb20bb0e2afdc27e
YirenNing24 <paulanthonyarriola@gmail.com>
2024-12-30 03:34:04 +0800

    first commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "BeatsPassKeyIOS",
+	platforms: [
+		.iOS(.v15) // Specify the minimum iOS version
+	],
+	products: [
+		.library(
+			name: "BeatsPassKeyIOS",
+			type: .dynamic,
+			targets: ["BeatsPassKeyIOS"]
+		),
+	],
+	dependencies: [
+		.package(url: "https://github.com/migueldeicaza/SwiftGodotKit.git", branch: "main")
+	],
+	targets: [
+		.target(
+			name: "BeatsPassKeyIOS",
+			dependencies: [
+				"SwiftGodot"
+			],
+			swiftSettings: [
+				.unsafeFlags(["-suppress-warnings"])
+			]
+		),
+		.testTarget(
+			name: "BeatsPassKeyIOSTests",
+			dependencies: ["BeatsPassKeyIOS"]
+		),
+	]
+)
