e262e4bf260feabc0852b3b06a23c21986b3a7e7
YirenNing24 <paulanthonyarriola@gmail.com>
2024-12-31 00:32:07 +0800

    updates

--- Sources/BeatsPassKeyIOS/BeatsPassKeyIOS.swift
+++ Sources/BeatsPassKeyIOS/BeatsPassKeyIOS.swift
@@ -18,27 +18,27 @@ class BeatsPassKeyIOS: RefCounted {
     #signal("create_passkey_completed", arguments: ["responseJson": String.self])
     #signal("create_passkey_error", arguments: ["errorMessage": String.self])
 
-    @Callable
-    func initiateSignInWithPasskey(requestJson: String) -> Void {
-        guard let requestData = requestJson.data(using: .utf8) else {
-            emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Invalid JSON data")
-            return
-        }
+    // @Callable
+    // func initiateSignInWithPasskey(requestJson: String) -> Void {
+    //     guard let requestData = requestJson.data(using: .utf8) else {
+    //         emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Invalid JSON data")
+    //         return
+    //     }
 
-        do {
-            let credentialRequest = try JSONDecoder().decode(ASAuthorizationPlatformPublicKeyCredentialDescriptor.self, from: requestData)
+    //     do {
+    //         let credentialRequest = try JSONDecoder().decode(ASAuthorizationPlatformPublicKeyCredentialDescriptor.self, from: requestData)
 
-            let request = ASAuthorizationPlatformPublicKeyCredentialProvider().createCredentialAssertionRequest(descriptor: credentialRequest)
+    //         let request = ASAuthorizationPlatformPublicKeyCredentialProvider().createCredentialAssertionRequest(descriptor: credentialRequest)
 
-            self.authorizationController = ASAuthorizationController(authorizationRequests: [request])
-            self.authorizationController?.delegate = self
-            self.authorizationController?.presentationContextProvider = self
-            self.authorizationController?.performRequests()
+    //         self.authorizationController = ASAuthorizationController(authorizationRequests: [request])
+    //         self.authorizationController?.delegate = self
+    //         self.authorizationController?.presentationContextProvider = self
+    //         self.authorizationController?.performRequests()
 
-        } catch {
-            emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Failed to parse request JSON")
-        }
-    }
+    //     } catch {
+    //         emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Failed to parse request JSON")
+    //     }
+    // }
 
     @Callable
     func createPasskey(requestJson: String) -> Void {
@@ -63,29 +63,29 @@ class BeatsPassKeyIOS: RefCounted {
     }
 }
 
-// MARK: - ASAuthorizationControllerDelegate
-extension BeatsPassKeyIOS: ASAuthorizationControllerDelegate {
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        if let credential = authorization.credential as? ASAuthorizationPlatformPublicKeyCredential {
-            if let credentialData = credential.rawCredential,
-               let responseJson = String(data: credentialData, encoding: .utf8) {
-                emit(signal: BeatsPassKeyIOS.sign_in_passkey_completed, responseJson)
-            } else {
-                emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Failed to decode credential data")
-            }
-        } else {
-            emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Unexpected credential type")
-        }
-    }
+// // MARK: - ASAuthorizationControllerDelegate
+// extension BeatsPassKeyIOS: ASAuthorizationControllerDelegate {
+//     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+//         if let credential = authorization.credential as? ASAuthorizationPlatformPublicKeyCredential {
+//             if let credentialData = credential.rawCredential,
+//                let responseJson = String(data: credentialData, encoding: .utf8) {
+//                 emit(signal: BeatsPassKeyIOS.sign_in_passkey_completed, responseJson)
+//             } else {
+//                 emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Failed to decode credential data")
+//             }
+//         } else {
+//             emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, "Unexpected credential type")
+//         }
+//     }
 
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, error.localizedDescription)
-    }
-}
+//     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+//         emit(signal: BeatsPassKeyIOS.sign_in_passkey_error, error.localizedDescription)
+//     }
+// }
 
-// MARK: - ASAuthorizationControllerPresentationContextProviding
-extension BeatsPassKeyIOS: ASAuthorizationControllerPresentationContextProviding {
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return UIApplication.shared.windows.first { $0.isKeyWindow } ?? ASPresentationAnchor()
-    }
-}
+// // MARK: - ASAuthorizationControllerPresentationContextProviding
+// extension BeatsPassKeyIOS: ASAuthorizationControllerPresentationContextProviding {
+//     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+//         return UIApplication.shared.windows.first { $0.isKeyWindow } ?? ASPresentationAnchor()
+//     }
+// }
