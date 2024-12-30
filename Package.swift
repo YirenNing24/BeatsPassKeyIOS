5af40283ee0c4879357de522394b1408acf0844f
paulmac <paulanthony@gmetarave.com>
2024-12-30 04:10:36 -0800

    updates

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.10
-
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "BeatsPassKeyIOS",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .macOS(.v10)
     ],
     products: [
         .library(
